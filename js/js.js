c412a4cc21447324a8404ee4ffc83ef8c740efed
jaredoneal <joneal12@uncc.edu>
2017-11-14 01:10:38 -0500

    third module test 3

--- js/js.js
+++ js/js.js
@@ -2,6 +2,7 @@ function MenuChoice(selection) {
   document.getElementById("customerList").style.visibility = "hidden";
   document.getElementById("orderHistory").style.visibility = "hidden";
   document.getElementById("aboutUs").style.visibility = "hidden";
+  document.getElementById("updateCustomerOrder").style.visibility = "hidden";
   switch (selection) {
     case "customerList":
       document.getElementById("customerList").style.visibility = "visible";
@@ -12,6 +13,13 @@ function MenuChoice(selection) {
       break;
     case "aboutUs":
       document.getElementById("aboutUs").style.visibility = "visible";
+      break;
+    case "customerOrders":
+      document.getElementById("customerOrders").style.visibility = "visible";
+      break;
+    case "updateCustomerOrder":
+      document.getElementById("updateCustomerOrder").style.visibility =
+        "visible";
       break;
     case "None": //No menu item selected, so no section should be displayed
       break;
@@ -66,10 +74,42 @@ function listStores() {
   }
 }
 
+// function Orders() {
+//   var xmlhttp = new XMLHttpRequest();
+//   var url =
+//     "https://student.business.uab.edu/jsonwebservice/service1.svc/getCustomerOrderHistory/";
+//   url += document.getElementById("CustomerIDInput").value;
+
+//   xmlhttp.onreadystatechange = function() {
+//     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+//       var output = JSON.parse(xmlhttp.responseText);
+//       GenerateOutput(output);
+//     }
+//   };
+//   xmlhttp.open("GET", url, true);
+//   xmlhttp.send();
+
+//   function GenerateOutput(result) {
+//     var display = "<table><tr><th>Total</th><th>Product Name</th></tr>";
+//     var count = 0;
+//     for (count = 0; count < result.length; count++) {
+//       display +=
+//         "<tr><td>" +
+//         result[count].Total +
+//         "</td><td>" +
+//         result[count].ProductName +
+//         "</td></tr>";
+//     }
+//     display += "</table>";
+//     document.getElementById("orderReport").innerHTML = display;
+//     // MenuChoice("orderHistory");
+//   }
+// }
+
 function Orders() {
   var xmlhttp = new XMLHttpRequest();
   var url =
-    "https://student.business.uab.edu/jsonwebservice/service1.svc/getCustomerOrderHistory/";
+    "https://student.business.uab.edu/jsonwebservice/service1.svc/getOrdersForCustomer/";
   url += document.getElementById("CustomerIDInput").value;
 
   xmlhttp.onreadystatechange = function() {
@@ -82,14 +122,21 @@ function Orders() {
   xmlhttp.send();
 
   function GenerateOutput(result) {
-    var display = "<table><tr><th>Total</th><th>Product Name</th></tr>";
+    var display =
+      "<table><tr><th>OrderID</th><th>Ship City</th><th>Ship Name</th><th>Ship Post Code</th><th>Ship Address</th></tr>";
     var count = 0;
-    for (count = 0; count < result.length; count++) {
+    for (count = 0; count < result.GetOrdersForCustomerResult.length; count++) {
       display +=
         "<tr><td>" +
-        result[count].Total +
+        result.GetOrdersForCustomerResult[count].OrderID +
         "</td><td>" +
-        result[count].ProductName +
+        result.GetOrdersForCustomerResult[count].ShipCity +
+        "</td><td>" +
+        result.GetOrdersForCustomerResult[count].ShipName +
+        "</td><td>" +
+        result.GetOrdersForCustomerResult[count].ShipPostCode +
+        "</td><td>" +
+        result.GetOrdersForCustomerResult[count].ShipAddress +
         "</td></tr>";
     }
     display += "</table>";
@@ -101,7 +148,7 @@ function Orders() {
 function OrdersWithParameters(CustomerID) {
   var xmlhttp = new XMLHttpRequest();
   var url =
-    "https://student.business.uab.edu/jsonwebservice/service1.svc/getCustomerOrderHistory/";
+    "https://student.business.uab.edu/jsonwebservice/service1.svc/getOrdersForCustomer/";
   url += CustomerID;
 
   xmlhttp.onreadystatechange = function() {
@@ -114,19 +161,122 @@ function OrdersWithParameters(CustomerID) {
   xmlhttp.send();
 
   function GenerateOutput(result) {
-    var display = "<table><tr><th>Total</th><th>Product Name</th></tr>";
+    var display =
+      "<table><tr><th>Update Order Button</tr></th><tr><th>OrderID</th><th>Ship City</th><th>Ship Name</th><th>Ship Post Code</th><th>Ship Address</th></tr>";
     var count = 0;
-    for (count = 0; count < result.length; count++) {
+    var orderIDParameter = "";
+    for (count = 0; count < result.GetOrdersForCustomerResult.length; count++) {
+      orderIDParameter = result.GetOrdersForCustomerResult[count].OrderID;
       display +=
         "<tr><td>" +
-        result[count].Total +
+        `<button onclick="getOrderInfo(${orderIDParameter})">Update Order Information</button>` +
         "</td><td>" +
-        result[count].ProductName +
+        result.GetOrdersForCustomerResult[count].OrderID +
+        "</td><td>" +
+        result.GetOrdersForCustomerResult[count].ShipCity +
+        "</td><td>" +
+        result.GetOrdersForCustomerResult[count].ShipName +
+        "</td><td>" +
+        result.GetOrdersForCustomerResult[count].ShipPostCode +
+        "</td><td>" +
+        result.GetOrdersForCustomerResult[count].ShipAddress +
         "</td></tr>";
     }
     display += "</table>";
     document.getElementById("orderReport").innerHTML = display;
     MenuChoice("orderHistory");
+  }
+}
+
+function getOrderInfo(
+  orderID
+) 
+//Retrieves a list of books ordered by a particular store using the store ID for the search
+{
+  debugger
+  var xmlhttp = new XMLHttpRequest();
+  var url =
+    "https://student.business.uab.edu/jsonwebservice/service1.svc/getCustomerOrderInfo/";
+  //Service URL
+  url += orderID;
+  //Store ID to complete Service URL
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      var output = JSON.parse(xmlhttp.responseText);
+      GenerateOutput(output);
+    }
+  };
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+  function GenerateOutput(result) //Function that displays results
+  {
+    result = result[0];
+    var resultOrderID = result.OrderID;
+    var resultShipAddress = result.ShipAddress;
+    var resultShipName = result.ShipName;
+    var resultShipCity = result.ShipCity;
+    var resultShipPostCode = result.ShipPostCode;
+    document.getElementById("orderID").value = resultOrderID;
+    document.getElementById("shipAddress").value = resultShipAddress;
+    document.getElementById("shipName").value = resultShipName;
+    document.getElementById("shipCity").value = resultShipCity;
+    document.getElementById("shipPostCode").value = resultShipPostCode;
+  }
+  MenuChoice("updateCustomerOrder");
+}
+
+function customerUpdate() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      var result = JSON.parse(xmlhttp.responseText);
+      OperationResult(Number(result));
+      //Calls the funciton that displays the result in an alert message
+      MenuChoice("customerList"); //Calls the menu choice function to display the store list
+    }
+  };
+  var url =
+    "https://student.business.uab.edu/jsonwebservice/service1.svc/UpdateOrderAddress";
+  var orderID = Number(document.getElementById("orderID").value);
+  var shipName = document.getElementById("shipName").value;
+  var shipAddress = document.getElementById("shipAddress").value;
+  var shipCity = document.getElementById("shipCity").value;
+  var shipPostCode = document.getElementById("shipPostCode").value;
+  var parameters =
+  JSON.stringify({
+    orderID,
+    shipName,
+    shipCity,
+    shipAddress,
+    shipPostCode
+  });
+  //Creates the JSON string to be sent for the update operation
+  xmlhttp.open("POST", url, true);
+  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xmlhttp.send(parameters);
+}
+//Function that displays the result of an operation that adds, deletes, or updates data
+//The function is invoked from other functions
+function OperationResult(success, exception) {
+  switch (success) {
+    case 1:
+      alert("The operation was successful");
+      break;
+    case 0:
+      alert("The operation was not successful: " + exception);
+      break;
+    case -2:
+      alert(
+        "The operation was not successful because the data string supplied could not be deserialized into the service object."
+      );
+      break;
+    case -3:
+      alert(
+        "The operation was not successful because a record with the supplied Order ID could not be found"
+      );
+      break;
+    default:
+      alert("The operation code returned is not identifiable.");
   }
 }
 

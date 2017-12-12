a9aac9a315b7e64be49ad7ab4a2a35432a0eb7bc
jaredoneal <joneal12@uncc.edu>
2017-12-11 20:40:25 -0600

    12/11

--- js/js.js
+++ js/js.js
@@ -455,8 +455,8 @@ function CapturePhoto() {
 
 function onSuccess(imageURI) {
   var picdisplay = document.getElementById("snapshot");
-  pickdisplay.style.display = "block";
-  pickdisplay.src = imageURI; //Assigns the picture to the image source property of the image on the web page
+  picdisplay.style.display = "block";
+  picdisplay.src = imageURI; //Assigns the picture to the image source property of the image on the web page
 }
 
 function onFail(message) {

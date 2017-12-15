c02322fb21989ce8cd2d950e8d5dc4750050173e
jaredoneal <joneal12@uncc.edu>
2017-12-14 20:55:44 -0600

    12/14

--- js/js.js
+++ js/js.js
@@ -618,17 +618,21 @@ function vibrate() {
   navigator.vibrate(2000);
 }
 
-window.addEventListener("batterystatus", onBatteryStatus, false);
+function checkConnection() {
+    var networkState = navigator.connection.type;
+ 
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+ 
+    alert('Connection type: ' + states[networkState]);
 
-function onBatteryStatus(status) {
-    console.log("Level: " + status.level + " isPlugged: " + status.isPlugged);
+    document.getElementById("networkStatus").innerHTML = states[networkState];
 }
 
-function batteryStatus(status) {
-  var level = status.level
-  var isPlugged = status.isPlugged;
-  document.getElementById("batteryStatus").innerhtml = level;
-  document.getElementById("batteryStatus").innerhtml += isPlugged;
-      console.log("Level: " + status.level + " isPlugged: " + status.isPlugged);
-
-}
